7ac5df274a04463758816b7c91f6c65f00ea1f4f
fabio <fabio@kreisern.eu>
2023-10-04 21:54:31 +0200

    added a logout frontent function

---
+++ client/src/components/Logout.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // For react-router v6
+
+const Logout: React.FC = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        localStorage.removeItem('token'); // Remove token from localStorage
+        navigate('/login'); // Navigates to login page after logout
+    }, [navigate]);
+
+    // Render nothing, or a loading spinner, or any other placeholder
+    return null;
+};
+
+export default Logout;
