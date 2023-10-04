b156c85978227fb1d0cff7567461874aef6408d8
fabio <fabio@kreisern.eu>
2023-10-04 21:56:24 +0200

    removed next function, after valid access token (understood the function wrong)

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -14,8 +14,10 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
 
   try {
     verify_access_token(access_token);
-    next();
+    console.log("access token is valid")
   } catch (e) {
     next(createError.Unauthorized(""));
   }
+
+  return "";
 };
