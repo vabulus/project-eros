7af928fd5d063eee1250d953b391b95c956857cf
fabio <fabio@kreisern.eu>
2023-10-04 21:58:51 +0200

    added new services function for retrieving user profile data

---
+++ server/src/userProfileServices.ts
@@ -0,0 +1,13 @@
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+export async function get_user_details(username: string, _email: string) {
+    return prisma.user.findUnique({
+        where: {
+            email: _email,
+        },
+        select: {email: true, username: true}
+    });
+}
