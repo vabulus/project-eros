3bccd450b19f72b8a4d8b2f713aca1b406601561
fabio <fabio@kreisern.eu>
2023-10-05 15:17:07 +0200

    uncommited dirs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,25 @@
-import React from "react";
+import React, {lazy} from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import LoginForm from "./components/LoginForm";
 import Profile from "./components/Profile";
-import ProtectedRoute from "./components/ProtectedRoute"; // Make sure to import this
 import RegistrationForm from "./components/RegistrationForm";
+import Logout from "./components/Logout";
+import StartPage from "./components/StartPage";
+
+import "./styles/index.css";
+import "./styles/App.css";
 
 const App: React.FC = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<StartPage />} />
           <Route path="/register" element={<RegistrationForm />} />
           <Route path="/login" element={<LoginForm />} />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/logout" element={<Logout />} />
         </Routes>
       </div>
     </Router>
