9ddf408cdf38eee0f25a68c17312cda5d2aa28eb
fabio <fabio@kreisern.eu>
2023-10-15 21:07:17 +0200

    cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,24 @@
 import React, {lazy} from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import LoginForm from "./components/LoginForm";
-import Profile from "./components/Profile";
-import RegistrationForm from "./components/RegistrationForm";
-import Logout from "./components/Logout";
-import StartPage from "./components/StartPage";
+import LoginForm from "./pages/LoginForm";
+import Profile from "./pages/Profile";
+import RegistrationForm from "./pages/RegistrationForm";
+import Logout from "./pages/Logout";
+import Dashboard from "./components/dashboard";
+
 
 import "./styles/index.css";
 import "./styles/App.css";
+import MultiStepForm2 from "./components/multiStepForm2";
 
 const App: React.FC = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<StartPage />} />
+          <Route path="/2" element={<MultiStepForm2/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
           <Route path="/register" element={<RegistrationForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/profile" element={<Profile />} />
